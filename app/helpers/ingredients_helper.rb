fc8e4428b11ccb6317f00d57526ce4e9545cc070
Tony Dujardin <midor4@gmail.com>
2012-02-19 20:32:27 +0100

    checkbox + list en commentaire

--- app/helpers/ingredients_helper.rb
+++ app/helpers/ingredients_helper.rb
@@ -1,2 +1,12 @@
 module IngredientsHelper
+  
+  def bon_ingredient?(ingredient)
+    if @pizz && !@pizz.name.nil? # no sense in testing new users that have no languages
+        @pizz.ingredient.include?(ingredient)
+    else
+        false
+    end
+end
+  
+  
 end
